de447e953730d48af8d5ca96940ceb39b0aee36c
Johan  Esteven <100029638+JohanEstev@users.noreply.github.com>
2022-03-03 18:47:42 -0500

    Add files via upload

---
+++ ejercios.py
@@ -0,0 +1,36 @@
+#def perimetrocuadrado():
+
+    #lado=float(input("Ingrese el valor del lado: "))
+    #print("perimetro del  cuadrado es ...",lado*4 )
+    
+#perimetrocuadrado()
+
+
+
+
+#def perimetrorectangulo():
+    
+    #base=float(input("Ingrese el valor de la base: "))
+    #altura=float(input("Ingrese el valor de la altura: "))
+    
+    #perimetro = (2*base)+(2*altura)
+    
+    #print("El perimetro del rectangulo es: " , perimetro)
+          
+#perimetrorectangulo()
+
+
+
+
+def perimetrocirculo():
+
+    radio=float(input("Ingrese el valor del radio: "))
+ 
+    pi=3.1416
+ 
+    perimetro=2*pi*radio
+    
+    print("El perimetro  del circulo  es...",perimetro)
+ 
+    
+perimetrocirculo()
