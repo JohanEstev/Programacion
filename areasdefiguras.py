ebdc91fca81191b20f69299feff4b43b1460a1b3
Johan  Esteven <100029638+JohanEstev@users.noreply.github.com>
2022-03-02 20:35:48 -0500

    Add files via upload

---
+++ areasdefiguras.py
@@ -0,0 +1,26 @@
+#def areacuadrado ():
+   # lado=float(input("Ingrese el valor  "))
+    #print("El area del cuadrado es ..", lado**2)
+    
+    
+#areacuadrado()
+
+
+
+def arearectangulo():
+    base=float(input("Inrese el valor: "))
+    altura=float(input("Inrese el valor: "))
+
+    print("el area del rectangulo es ...", 2*(base+altura)
+    
+    
+arearectangulo()
+
+
+
+#def areacirculo():
+    #radio=float(input("Ingrese el valor del radio: "))
+    #pi=3.1416
+    #print("area del circulo es...",pi*radio**2)
+    
+#areacirculo()
